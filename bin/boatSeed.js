313b64b406076fc6a2bf97a45fb22a58fe8cacc5
laanayam333 <laanayam333@gmail.com>
2019-08-08 17:05:54 +0200

    map colors etc etc etc etc

--- bin/boatSeed.js
+++ bin/boatSeed.js
@@ -11,8 +11,8 @@ const boats = [
     type: "Yacht",
     capacity: 10,
     captain: true,
-    owner: "5d49547de191c926fc287b0e",
-    port: "5d494c21c9ac6017404671c2",
+    owner: "5d4c00689027753da0d2fb4d",
+    port: "5d494c21c9ac6017404671bd",
     description: "Perfect for a party.",
     rate: 1000,
     imgPath:
@@ -24,8 +24,8 @@ const boats = [
     type: "Sailboat",
     capacity: 4,
     captain: false,
-    owner: "5d49547de191c926fc287b0e",
-    port: "5d494c21c9ac6017404671c2",
+    owner: "5d4c00689027753da0d2fb4d",
+    port: "5d494c21c9ac6017404671bd",
     description: "Enjoy a windy day at the bay.",
     rate: 400,
     imgPath:
@@ -37,8 +37,8 @@ const boats = [
     type: "Catamaran",
     capacity: 6,
     captain: true,
-    owner: "5d49547de191c926fc287b0e",
-    port: "5d494c21c9ac6017404671c2",
+    owner: "5d4c00689027753da0d2fb4d",
+    port: "5d494c21c9ac6017404671bd",
     description: "Scuba diving equipment included.",
     rate: 1200,
     imgPath:
@@ -46,227 +46,227 @@ const boats = [
     imgName: "3_catamaran.jpg"
   },
 
-  // {
-  //   name: "Unsinkable II",
-  //   type: "Yacht",
-  //   capacity: 5,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d494c21c9ac6017404671c2",
-  //   description: "Relax while feeling super safe.",
-  //   rate: 900,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565027559/folder-name/4_yacht.jpg.jpg",
-  //   imgName: "4_yacht.jpg"
-  // },
-  // {
-  //   name: "Cupid",
-  //   type: "Sailboat",
-  //   capacity: 2,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4b6",
-  //   description: "Enjoy a romantic evening with your SO.",
-  //   rate: 200,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565027827/folder-name/5_sailboat.png.png",
-  //   imgName: "5_sailboat.png"
-  // },
-  // {
-  //   name: "Seaduction",
-  //   type: "Catamaran",
-  //   capacity: 4,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4b8",
-  //   description: "The double date champion.",
-  //   rate: 900,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028109/folder-name/6_catamaran.jpg.jpg",
-  //   imgName: "6_catamaran.jpg"
-  // },
-  // {
-  //   name: "Big Nauti",
-  //   type: "Yacht",
-  //   capacity: 15,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4b9",
-  //   description: "Celebrate your special day with us.",
-  //   rate: 2000,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028288/folder-name/7_yacht.jpg.jpg",
-  //   imgName: "7_yacht.jpg"
-  // },
-  // {
-  //   name: "The Midget",
-  //   type: "Sailboat",
-  //   capacity: 3,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4ba",
-  //   description: "Get close with those that you love.",
-  //   rate: 300,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028685/folder-name/8_sailboat.jpg.jpg",
-  //   imgName: "8_sailboat.jpg"
-  // },
-  // {
-  //   name: "Fango Fandango",
-  //   type: "Catamaran",
-  //   capacity: 4,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4be",
-  //   description: "For professional adrelanine seekers.",
-  //   rate: 600,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029094/folder-name/9_catamaran.jpg.jpg",
-  //   imgName: "1_yacht.jpg"
-  // },
-  // {
-  //   name: "Helios",
-  //   type: "Yacht",
-  //   capacity: 7,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4bf",
-  //   description: "Family friendly. Lunch included.",
-  //   rate: 800,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029376/folder-name/10_yacht.jpg.jpg",
-  //   imgName: "10_yacht.jpg"
-  // },
-  // {
-  //   name: "Popeye",
-  //   type: "Sailboat",
-  //   capacity: 1,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4c0",
-  //   description: "Fly with the wind.",
-  //   rate: 100,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029511/folder-name/11_sailboat.jpg.jpg",
-  //   imgName: "11_sailboat.jpg"
-  // },
-  // {
-  //   name: "La Dolce Vita",
-  //   type: "Catamaran",
-  //   capacity: 4,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4c3",
-  //   description: "Get away for a while and enjoy life.",
-  //   rate: 1000,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029877/folder-name/12_catamaran.jpg.jpg",
-  //   imgName: "12_catamaran.jpg"
-  // },
-  // {
-  //   name: "Boaty McBoatface",
-  //   type: "Yacht",
-  //   capacity: 4,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4c5",
-  //   description: "Water. Drinks. Sexy times.",
-  //   rate: 1200,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565030294/folder-name/13_yacht.jpg.jpg",
-  //   imgName: "13_yacht.jpg"
-  // },
-  // {
-  //   name: "Titanic",
-  //   type: "Sailboat",
-  //   capacity: 3,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4c6",
-  //   description: "Come sightseeing with us.",
-  //   rate: 200,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031736/folder-name/14_sailboat.jpg.jpg",
-  //   imgName: "14_sailboat.jpg"
-  // },
-  // {
-  //   name: "Santa Maria",
-  //   type: "Catamaran",
-  //   capacity: 6,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4c9",
-  //   description: "Enjoy the sun and the sea.",
-  //   rate: 1100,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031855/folder-name/15_catamaran.jpg.jpg",
-  //   imgName: "15_catamaran.jpg"
-  // },
-  // {
-  //   name: "Hobbit",
-  //   type: "Yacht",
-  //   capacity: 2,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4ca",
-  //   description: "Sail into the sunset.",
-  //   rate: 300,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031906/folder-name/16_yacht.jpg.jpg",
-  //   imgName: "16_yacht.jpg"
-  // },
-  // {
-  //   name: "Castaway",
-  //   type: "Sailboat",
-  //   capacity: 3,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4cb",
-  //   description: "Get lost for how long as you want.",
-  //   rate: 500,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031970/folder-name/17_sailboat.jpg.jpg",
-  //   imgName: "17_sailboat.jpg"
-  // },
-  // {
-  //   name: "The Churchill",
-  //   type: "Catamaran",
-  //   capacity: 4,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4cd",
-  //   description: "Regal entertainment.",
-  //   rate: 900,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565042970/folder-name/18_catamaran.jpg.jpg",
-  //   imgName: "18_catamaran.jpg"
-  // },
-  // {
-  //   name: "Mediterráneo",
-  //   type: "Yacht",
-  //   capacity: 4,
-  //   captain: true,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4ce",
-  //   description: "Listen to Serrat on a loop.",
-  //   rate: 1100,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565043114/folder-name/19_yacht.jpg.jpg",
-  //   imgName: "19_yacht.jpg"
-  // },
-  // {
-  //   name: "Lemon Drizzle",
-  //   type: "Sailboat",
-  //   capacity: 2,
-  //   captain: false,
-  //   owner: "5d49547de191c926fc287b0e",
-  //   port: "5d485b81628f651e5029f4d2",
-  //   description: "Perfect for a romantic afternoon in the bay.",
-  //   rate: 400,
-  //   imgPath:
-  //     "http://res.cloudinary.com/dqfytofoz/image/upload/v1565043186/folder-name/20_sailboat.jpg.jpg",
-  //   imgName: "20_sailboat.jpg"
-  // }
+  {
+    name: "Unsinkable II",
+    type: "Yacht",
+    capacity: 5,
+    captain: true,
+    owner: "5d4c00689027753da0d2fb4d",
+    port: "5d494c21c9ac6017404671c2",
+    description: "Relax while feeling super safe.",
+    rate: 900,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565027559/folder-name/4_yacht.jpg.jpg",
+    imgName: "4_yacht.jpg"
+  },
+  {
+    name: "Cupid",
+    type: "Sailboat",
+    capacity: 2,
+    captain: false,
+    owner: "5d4c00689027753da0d2fb4d",
+    port: "5d485b81628f651e5029f4b6",
+    description: "Enjoy a romantic evening with your SO.",
+    rate: 200,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565027827/folder-name/5_sailboat.png.png",
+    imgName: "5_sailboat.png"
+  },
+  {
+    name: "Seaduction",
+    type: "Catamaran",
+    capacity: 4,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4b8",
+    description: "The double date champion.",
+    rate: 900,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028109/folder-name/6_catamaran.jpg.jpg",
+    imgName: "6_catamaran.jpg"
+  },
+  {
+    name: "Big Nauti",
+    type: "Yacht",
+    capacity: 15,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4b9",
+    description: "Celebrate your special day with us.",
+    rate: 2000,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028288/folder-name/7_yacht.jpg.jpg",
+    imgName: "7_yacht.jpg"
+  },
+  {
+    name: "The Midget",
+    type: "Sailboat",
+    capacity: 3,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4ba",
+    description: "Get close with those that you love.",
+    rate: 300,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565028685/folder-name/8_sailboat.jpg.jpg",
+    imgName: "8_sailboat.jpg"
+  },
+  {
+    name: "Fango Fandango",
+    type: "Catamaran",
+    capacity: 4,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4be",
+    description: "For professional adrelanine seekers.",
+    rate: 600,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029094/folder-name/9_catamaran.jpg.jpg",
+    imgName: "1_yacht.jpg"
+  },
+  {
+    name: "Helios",
+    type: "Yacht",
+    capacity: 7,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4bf",
+    description: "Family friendly. Lunch included.",
+    rate: 800,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029376/folder-name/10_yacht.jpg.jpg",
+    imgName: "10_yacht.jpg"
+  },
+  {
+    name: "Popeye",
+    type: "Sailboat",
+    capacity: 1,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4c0",
+    description: "Fly with the wind.",
+    rate: 100,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029511/folder-name/11_sailboat.jpg.jpg",
+    imgName: "11_sailboat.jpg"
+  },
+  {
+    name: "La Dolce Vita",
+    type: "Catamaran",
+    capacity: 4,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4c3",
+    description: "Get away for a while and enjoy life.",
+    rate: 1000,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565029877/folder-name/12_catamaran.jpg.jpg",
+    imgName: "12_catamaran.jpg"
+  },
+  {
+    name: "Boaty McBoatface",
+    type: "Yacht",
+    capacity: 4,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4c5",
+    description: "Water. Drinks. Sexy times.",
+    rate: 1200,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565030294/folder-name/13_yacht.jpg.jpg",
+    imgName: "13_yacht.jpg"
+  },
+  {
+    name: "Titanic",
+    type: "Sailboat",
+    capacity: 3,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4c6",
+    description: "Come sightseeing with us.",
+    rate: 200,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031736/folder-name/14_sailboat.jpg.jpg",
+    imgName: "14_sailboat.jpg"
+  },
+  {
+    name: "Santa Maria",
+    type: "Catamaran",
+    capacity: 6,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4c9",
+    description: "Enjoy the sun and the sea.",
+    rate: 1100,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031855/folder-name/15_catamaran.jpg.jpg",
+    imgName: "15_catamaran.jpg"
+  },
+  {
+    name: "Hobbit",
+    type: "Yacht",
+    capacity: 2,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4ca",
+    description: "Sail into the sunset.",
+    rate: 300,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031906/folder-name/16_yacht.jpg.jpg",
+    imgName: "16_yacht.jpg"
+  },
+  {
+    name: "Castaway",
+    type: "Sailboat",
+    capacity: 3,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4cb",
+    description: "Get lost for how long as you want.",
+    rate: 500,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565031970/folder-name/17_sailboat.jpg.jpg",
+    imgName: "17_sailboat.jpg"
+  },
+  {
+    name: "The Churchill",
+    type: "Catamaran",
+    capacity: 4,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4cd",
+    description: "Regal entertainment.",
+    rate: 900,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565042970/folder-name/18_catamaran.jpg.jpg",
+    imgName: "18_catamaran.jpg"
+  },
+  {
+    name: "Mediterráneo",
+    type: "Yacht",
+    capacity: 4,
+    captain: true,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4ce",
+    description: "Listen to Serrat on a loop.",
+    rate: 1100,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565043114/folder-name/19_yacht.jpg.jpg",
+    imgName: "19_yacht.jpg"
+  },
+  {
+    name: "Lemon Drizzle",
+    type: "Sailboat",
+    capacity: 2,
+    captain: false,
+    owner: "5d49547de191c926fc287b0e",
+    port: "5d485b81628f651e5029f4d2",
+    description: "Perfect for a romantic afternoon in the bay.",
+    rate: 400,
+    imgPath:
+      "http://res.cloudinary.com/dqfytofoz/image/upload/v1565043186/folder-name/20_sailboat.jpg.jpg",
+    imgName: "20_sailboat.jpg"
+  }
 ];
 
 Boat.create(boats, err => {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,362 @@
 window.onload = () => {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4.3,
-    center: { lat: 41.38623, lng: 2.17498 }
+    center: { lat: 40.38623, lng: 6.17498 },
+    zoom: 5.4,
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#ebe3cd"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#F6F7BD"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#f5f1e6"
+          }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#c9b2a6"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.country",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#dcd2be"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#ae9e90"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.locality",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.neighborhood",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.province",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "landscape.man_made",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#93B1AF"
+          }
+        ]
+      },
+      {
+        featureType: "landscape.natural.terrain",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#93817c"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#a5b076"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#447530"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#f5f1e6"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#fdfcf8"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#f8c967"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#e9bc62"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#e98d58"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#db8555"
+          }
+        ]
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#806b63"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        stylers: [
+          {
+            color: "#f8f596"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#E6AC27"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#f8f596"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#8f7d77"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ebe3cd"
+          }
+        ]
+      },
+      {
+        featureType: "transit.station",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#5B5F60"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#5B5F60"
+          }
+        ]
+      }
+    ]
   });
 
-  document.getElementById("elboton").onclick = () => getPorts(map)
-}
+  document.getElementById("elboton").onclick = () => {
+    getPorts(map);
+  };
+};
 
 function getPorts(map) {
-  const infoId = document.getElementById("homeport-list").value //el valor sale del formulario
+  const infoId = document.getElementById("homeport-list").value; //el valor sale del formulario
   console.log({ infoId });
-  axios.post('/user/search', { infoId }) //es la misma dirección que la indicada en user.routers '/'
-    .then(response => {      //response es el valor de thePort de user.routers
+  axios
+    .post("/user/search", { infoId }) //es la misma dirección que la indicada en user.routers '/'
+    .then(response => {
+      //response es el valor de thePort de user.routers
       console.log(response.data); //con .data accedemos a toda la info que contiene el port en la BBDD
-      let parent = document.getElementById("prueba")
+      let parent = document.getElementById("prueba");
       response.data.thePort.boats.forEach(el => {
-        console.log(el)
-        let newElement = document.createElement("div")
-        let name = document.createElement("div")
-        newElement.innerHTML = el._id
-        name.innerHTML = el.name
+        console.log(el);
+        let newElement = document.createElement("div");
+        let name = document.createElement("div");
+        newElement.innerHTML = el._id;
+        name.innerHTML = el.name;
 
-        parent.appendChild(newElement)
-        parent.appendChild(name)
-
-      })
+        parent.appendChild(newElement);
+        parent.appendChild(name);
+      });
       placePorts(response, map);
     })
     .catch(err => console.log(err));
@@ -36,9 +368,33 @@ function placePorts(response, map) {
     lng: response.data.thePort.location.coordinates[0]
   };
 
-  new google.maps.Marker({
+  map.setCenter(locatedAt);
+  map.setZoom(8);
+
+  const icon = {
+    url: "/images/boat-orange.png",
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+  };
+
+  const marker = new google.maps.Marker({
     position: locatedAt,
     map: map,
-    title: response.data.name
+    title: response.data.name,
+    icon: icon
+  });
+
+  marker.addListener("click", function() {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
+  });
+
+  map.addListener("center_changed", function() {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function() {
+      map.panTo(marker.getPosition());
+    }, 3000);
   });
 }
